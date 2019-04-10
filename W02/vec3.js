cebfe5d5610cbaaf2e08fab1b85723283ed7c59b
柏木　裕介 <191x205x@istc-a-025.epc.center.kobe-u.ac.jp>
2019-04-10 16:55:28 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,8 +1,7 @@
 Vec3 = function(x, y, z){
-  this.x = x;
-  this.y = y;
-  this.z = z;
-  this.array = [x, y, z];
+  this.x = parseInt(x);
+  this.y = parseInt(y);
+  this.z = parseInt(z);
 }
 
 Vec3.prototype.add = function(v){
@@ -12,11 +11,19 @@ Vec3.prototype.add = function(v){
   return this;
 }
 
+Vec3.prototype.sub = function(v){
+  this.x -= v.x
+  this.y -= v.y
+  this.z -= v.z
+  return this
+}
+
 Vec3.prototype.sum = function () {
   return this.x + this.y + this.z;
 }
 
 Vec3.prototype.min = function () {
+  this.array = [this.x, this.y, this.z];
   this.array.sort(function(a, b){
     if(a<b) return -1;
     if(a>b) return 1;
@@ -27,6 +34,7 @@ Vec3.prototype.min = function () {
 }
 
 Vec3.prototype.mid = function(){
+  this.array = [this.x, this.y, this.z];
   this.array.sort(function(a, b){
     if(a<b) return -1;
     if(a>b) return 1;
@@ -36,6 +44,7 @@ Vec3.prototype.mid = function(){
 }
 
 Vec3.prototype.max = function(){
+  this.array = [this.x, this.y, this.z];
   this.array.sort(function(a, b){
     if(a<b) return -1;
     if(a>b) return 1;
@@ -44,6 +53,6 @@ Vec3.prototype.max = function(){
   return this.array[2];
 }
 
-Vec3.prototype.resetArray = function(){
-  this.array = [this.x, this.y, this.z];
+Vec3.prototype.return = function(){
+  return [this.x, this.y, this.z];
 }
